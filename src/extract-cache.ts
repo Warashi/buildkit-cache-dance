ba4e2b98224a8e5791891d0ddef38dcffb0d9d4c
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-31 23:18:19 +0900

    wip

--- src/extract-cache.ts
+++ src/extract-cache.ts
@@ -19,7 +19,8 @@ FROM ${containerImage} AS extract
 COPY buildstamp buildstamp
 RUN --mount=${mountArgs} \
     mkdir -p /var/dance-cache/ \
-    && tar -cz -f /var/dance-cache/dance-cache.tar.gz -C '${targetPath}/.' './.'
+    && cd '${targetPath}/.'
+    && tar -cz -f /var/dance-cache/dance-cache.tar.gz '.'
 
 FROM scratch
 COPY --from=extract /var/dance-cache/dance-cache.tar.gz /dance-cache.tar.gz
