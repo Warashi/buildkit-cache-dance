666fc98ec1924f7f4debc8de022a29943e49b914
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-31 23:20:13 +0900

    fix: syntax error

--- src/extract-cache.ts
+++ src/extract-cache.ts
@@ -19,7 +19,7 @@ FROM ${containerImage} AS extract
 COPY buildstamp buildstamp
 RUN --mount=${mountArgs} \
     mkdir -p /var/dance-cache/ \
-    && cd '${targetPath}/.'
+    && cd '${targetPath}/.' \
     && tar -cz -f /var/dance-cache/dance-cache.tar.gz '.'
 
 FROM scratch
